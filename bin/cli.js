fa9aa18ac47831dc819e4e2694cda2014c5d6ade
Kevin O'Hara <kevinohara80@gmail.com>
2012-05-25 14:53:27 -0400

    got cli working

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,55 @@
 #!/usr/bin/env node
+
 var nforce = require('../');
-var argv = require('optimist').argv;
+var fs = require('fs');
+
+var optimist = require('optimist');
+
+var argv = optimist
+  .usage([
+    '',
+    '***************************',
+    '* nforce cli              *',
+    '***************************',
+    '',
+    '  USAGE:',
+    '',
+    '  $0 config   ::  manage your configuration',
+    '  $0 query    ::  execute a SOQL query',
+    '  $0 version  ::  show your nforce version',
+    '',
+    '-> for specific command help type `nforce help [command]`'
+  ].join('\r\n'))
+  .argv
+;
+
+var cmd = argv._[0];
+
+if(cmd == 'version') {
+  console.log('nforce version ' + nforce.version);
+} else if (cmd == 'help') {
+  if(argv._[1]) {
+    var helpCmd = argv._[1];
+    console.log(helpCmd);
+  } else {
+    optimist.showHelp();
+  }
+} else if(cmd == 'config') {
+  fs.open(__dirname + '/nforce.json', 'w+', function(err, file) {
+    if(err) return console.log('error accessing file');
+    console.log(__dirname + '/nforce.json');
+  });
+} else if(cmd == 'query') {
+  var config = fs.openSync(__dirname + '/nforce.json', 'w+');
+  console.log(config);
+} else {
+  if(cmd) {
+    console.error('`' + cmd + '` is not a supported command!');
+    console.log(' ');
+  }
+  optimist.showHelp();
+}
+
+
+
 
